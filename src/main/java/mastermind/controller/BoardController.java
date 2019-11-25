33bd81c184c99c0a992a2853cf841b0438d21293
Andrzej <andrzej.ratajczak98@gmail.com>
2019-11-25 17:53:31 +0100

    Added views and initialized controllers

--- src/main/java/mastermind/controller/BoardController.java
+++ src/main/java/mastermind/controller/BoardController.java
@@ -5,24 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import mastermind.Main;
-import mastermind.model.Board;
-import mastermind.model.CodeWord;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class BoardController extends AbstractController {
 
     private final int guessWidth = 4;
     private final int dispenserWidth = 1;
     private final int dispenserHeight = 8;
-
-    private Board board = new Board(new CodeWord(Arrays.asList(
-            Color.BLUE, Color.FUCHSIA, Color.CYAN, Color.ORANGE
-    )));
 
     @FXML
     private Pane guessPane;
@@ -35,11 +27,11 @@ public class BoardController extends AbstractController {
 
     @FXML GuessController guessController;
 
-    private DragHandler dragHandler = new DragHandler();
-
     @Override
     public void initLayout(Stage primaryStage) {
         try {
+            primaryStage.setTitle("MasterMind");
+
             FXMLLoader boardLoader = new FXMLLoader();
             boardLoader.setLocation(Main.class.getResource("view/BoardView.fxml"));
 
@@ -47,9 +39,11 @@ public class BoardController extends AbstractController {
             rootLayout.setMinHeight(100 * dispenserHeight);
             rootLayout.setMinWidth(100 * (dispenserWidth + guessWidth));
 
+
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
             primaryStage.show();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -59,8 +53,5 @@ public class BoardController extends AbstractController {
     protected void initialize() {
         guessPane.setLayoutX(100 * dispenserWidth);
         guessPane.setLayoutY(100 * (dispenserHeight - 1));
-
-        dispenserController.setDragHandler(dragHandler);
-        guessController.setDragHandler(dragHandler);
     }
 }
